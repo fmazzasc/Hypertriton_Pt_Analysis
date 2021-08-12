Multiple fixes + add iterative spectrum calculation

ml_analysis.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import sys
+sys.path.append('utils')
+import helpers as hp
+
 import os
 import pickle
 import warnings
@@ -19,33 +23,6 @@ from sklearn.model_selection import train_test_split
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
-
-def presel_eff_hist(df_list, col_name, split, cent_bins, bins):
-    # fill histograms (vs. ct and vs. pt)
-    bins_array = np.asarray(bins, dtype=float)
-    hist_eff = ROOT.TH1F(
-        f'fPreselEff_vs_{col_name}_{split}_{cent_bins[0]}_{cent_bins[1]}',
-        f'Preselection Efficiency, {split}, {cent_bins[0]}-{cent_bins[1]}%', len(bins) - 1, bins_array)
-    hist_gen = ROOT.TH1F('fPreselGen_vs_{col_name}', 'Gen', len(bins)-1, bins_array)
-
-    for val in df_list[0][col_name]:
-        hist_eff.Fill(val)
-    for val in df_list[1][col_name]:
-        hist_gen.Fill(val)
-
-    # compute efficiency and set properties
-    hist_eff.Divide(hist_eff, hist_gen, 1, 1, "B")
-    if col_name == 'ct':
-        hist_eff.GetXaxis().SetTitle('#it{c}t (cm)')
-    elif col_name == 'pt':
-        hist_eff.GetXaxis().SetTitle('#it{p}_{T} (GeV/#it{c})')
-    hist_eff.GetYaxis().SetTitle('Efficiency')
-    hist_eff.SetMinimum(0)
-    hist_eff.SetDrawOption("histo")
-    hist_eff.SetLineWidth(2)
-
-    # return histogram
-    return hist_eff
 
 
 parser = argparse.ArgumentParser(prog='ml_analysis', allow_abbrev=True)
@@ -71,6 +48,7 @@ OPTIMIZED = False
 TRAIN = args.train
 COMPUTE_SCORES_FROM_EFF = args.computescoreff
 # application
+
 APPLICATION = args.application
 
 # avoid pandas warning
@@ -88,6 +66,9 @@ with open(os.path.expandvars(config), 'r') as stream:
         print(exc)
 
 DATA_PATH = params['DATA_PATH']
+MERGE_SAMPLES = params['MERGE_SAMPLES']
+KINT7 = params['KINT7']
+
 MC_PATH = params['MC_SIGNAL_PATH']
 BKG_PATH = params['LS_BACKGROUND_PATH']
 CT_BINS = params['CT_BINS']
@@ -164,8 +145,8 @@ if TRAIN:
                 print(pt_bins_cent)
 
                 # fill histograms (vs. ct and vs. pt)
-                hist_eff_ct = presel_eff_hist([df_signal_cent, df_generated_cent], 'ct', split, cent_bins, ct_bins_cent)
-                hist_eff_pt = presel_eff_hist([df_signal_cent, df_generated_cent], 'pt', split, cent_bins, pt_bins_cent)
+                hist_eff_ct = hp.presel_eff_hist([df_signal_cent, df_generated_cent], 'ct', split, cent_bins, ct_bins_cent)
+                hist_eff_pt = hp.presel_eff_hist([df_signal_cent, df_generated_cent], 'pt', split, cent_bins, pt_bins_cent)
 
                 # plot histograms
                 if not os.path.isdir(f'{PLOT_DIR}/presel_eff'):
@@ -191,11 +172,7 @@ if TRAIN:
 
     # second condition needed because of issue with Qt libraries
     if MAKE_FEATURES_PLOTS and not MAKE_PRESELECTION_EFFICIENCY and not TRAIN:
-        ######################################################
-        # PLOT FEATURES DISTRIBUTIONS AND CORRELATIONS
-        ######################################################
 
-        # define tree handlers
 
         if len(background_tree_handler)>5*len(signal_tree_handler):
             background_tree_handler.shuffle_data_frame(size=5*len(signal_tree_handler))
@@ -345,6 +322,12 @@ if APPLICATION:
 
     for split in SPLIT_LIST:
         df_data = uproot.open(os.path.expandvars(DATA_PATH))['DataTable'].arrays(library="pd")
+        if KINT7:
+            df_data = df_data.query('trigger==9 or trigger ==1 or trigger==9+2 or trigger==1+2 or trigger==9+4 or trigger==1+4')
+        if MERGE_SAMPLES:
+            df_2015 = uproot.open(os.path.expandvars('/data/fmazzasc//PbPb_2body/DataTable_15o.root'))['DataTable'].arrays(library="pd")
+            df_data = pd.concat([df_data, df_2015])
+        
 
         split_ineq_sign = '> -0.1'
         if SPLIT:
@@ -360,7 +343,7 @@ if APPLICATION:
                 bin_model = f'{pt_bins[0]}_{pt_bins[1]}'
 
                 df_data_cent = df_data.query(
-                    f'Matter {split_ineq_sign} and centrality > {cent_bins[0]} and centrality < {cent_bins[1]} and pt > {pt_bins[0]} and pt < {pt_bins[1]} and ct < 35')
+                    f'Matter {split_ineq_sign} and centrality > {cent_bins[0]} and centrality <= {cent_bins[1]} and pt > {pt_bins[0]} and pt < {pt_bins[1]} and ct < 35')
 
                 model_hdl = ModelHandler()
 

pt_spectra.py
@@ -63,7 +63,7 @@ eff_cut_dict = pickle.load(open(res_dir + "/file_eff_cut_dict", "rb"))
 presel_eff_file = uproot.open(res_dir + '/PreselEff.root')
 signal_extraction_file = ROOT.TFile.Open(res_dir + '/SignalExtraction.root')
 signal_extraction_up = uproot.open(res_dir + '/SignalExtraction.root')
-absorption_correction_file = uproot.open("results/He3_abs.root")
+absorption_correction_file = uproot.open(f"results{RESULTS_SUBDIR}/He3_abs.root")
 
 pt_spectra_file = ROOT.TFile.Open(res_dir + '/pt_spectra.root', 'recreate')
 
@@ -102,38 +102,50 @@ for i_cent_bins, pt_bins_cent in enumerate(PT_BINS_CENT):
     
             bin = f'{split}_{cent_bins[0]}_{cent_bins[1]}_{pt_bins[0]}_{pt_bins[1]}'
             formatted_eff_cut = "{:.2f}".format(eff_cut_dict[bin])
-            # formatted_eff_cut = "0.80"
 
-            # look for plot with eff = eff_cut (or the nearest one)
             bkg_shape = 'pol1'
             eff_cut_increment = 0
             eff_cut_sign = -1
             signal_extraction_keys = signal_extraction_up[f"{bin}_{bkg_shape}"].keys()
-            while signal_extraction_keys.count(f"fInvMass_{formatted_eff_cut};1".encode())==0 and eff_cut_increment<20:
+            while signal_extraction_keys.count(f"fInvMass_{formatted_eff_cut};1".encode())==0 and eff_cut_increment<0.20:
                 if eff_cut_sign == -1:
                     eff_cut_increment += 0.01
                 eff_cut_sign *= -1
                 formatted_eff_cut = "{:.2f}".format(eff_cut_dict[bin]+eff_cut_increment*eff_cut_sign)
+            
+            if signal_extraction_keys.count(f"fInvMass_{formatted_eff_cut};1".encode())==0:
+                continue
 
             # get signal
             h_raw_yield = signal_extraction_file.Get(f'{bin}_{bkg_shape}/fRawYields;1')
+            h_histo = signal_extraction_file.Get(f'{bin}_{bkg_shape}/fInvMass_{formatted_eff_cut}')
+            h_histo.SetName(f'fInvMass_{formatted_eff_cut}_pt_{pt_bins[0]}_{pt_bins[1]}')
+            #########################
             eff_index = h_raw_yield.FindBin(float(formatted_eff_cut))
             raw_yield = h_raw_yield.GetBinContent(eff_index)
             raw_yield_error = h_raw_yield.GetBinError(eff_index)
+
+            dire = f'{split}_{cent_bins[0]}_{cent_bins[1]}'
+            pt_spectra_file.cd()
+            if not pt_spectra_file.GetListOfKeys().Contains(dire):
+                pt_spectra_file.mkdir(dire)
+            pt_spectra_file.cd(dire)
+            h_histo.Write()
 
 
             presel_eff_map = np.logical_and(presel_eff_bin_centers > pt_bins[0], presel_eff_bin_centers < pt_bins[1])
             absorption_map = np.logical_and(absorption_bin_centers > pt_bins[0], absorption_bin_centers < pt_bins[1])
 
             presel_eff = presel_eff_counts[presel_eff_map]
-            absorption_corr = absorption_counts[absorption_map]
-            absorption_corr = 0.96
+            absorption_corr = absorption_counts[absorption_map]*HYP_HE_CROSS_SECT_SCALING
+            # absorption_corr = 0.96
             bdt_eff = float(formatted_eff_cut)
             eff = presel_eff * eff_cut_dict[bin]
 
-            pt_bin_index = h_corrected_yields[i_split].FindBin(pt_bins[0]+0.5)
+            pt_bin_index = h_corrected_yields[i_split].FindBin(pt_bins[0]+0.2)
             h_corrected_yields[i_split].SetBinContent(pt_bin_index, raw_yield/eff[0]/absorption_corr)
             h_corrected_yields[i_split].SetBinError(pt_bin_index, raw_yield_error/eff[0]/absorption_corr)
+            print(pt_bin_index, raw_yield, eff[0])
             if SPLIT:
                 if i_split==0:
                     h_corrected_ratio.SetBinContent(pt_bin_index, raw_yield/eff[0]/absorption_corr)
@@ -149,7 +161,7 @@ for i_cent_bins, pt_bins_cent in enumerate(PT_BINS_CENT):
             print(f'bin: [{pt_bins[0]}, {pt_bins[1]}], BDT eff = {formatted_eff_cut}, presel_eff = {presel_eff}, raw_yield = {raw_yield}+{raw_yield_error}')
 
 
-        # set labels
+        # set label
         h_corrected_yields[i_split].GetXaxis().SetTitle("#it{p}_{T} (GeV/#it{c})")
         h_corrected_yields[i_split].GetYaxis().SetTitle("d#it{N}/d(y#it{p}_{T})")
 
@@ -163,8 +175,15 @@ for i_cent_bins, pt_bins_cent in enumerate(PT_BINS_CENT):
         h_corrected_yields[i_split].SetMarkerStyle(20)
         h_corrected_yields[i_split].SetMarkerSize(0.8)
 
+        if not pt_spectra_file.GetListOfKeys().Contains('results'):
+            pt_spectra_file.mkdir('results')
+        pt_spectra_file.cd('results')
+
         if FIT==True:
-            histo,Integral, integral_error = hp.bw_fit(h_corrected_yields[i_split], bw)
+            # histo,Integral, integral_error = hp.mtexpo_fit(h_corrected_yields[i_split])
+            histo,Integral, integral_error,_ = hp.bw_fit(h_corrected_yields[i_split], bw)
+            print("yield: ", Integral, ", error: ", integral_error)
+
             histo.Write()
     
     h_corrected_ratio.Write()

std_analysis/2015_std_analysis.py
@@ -32,18 +32,20 @@ piPtCuts = np.linspace(0.15, 0.18, 1)
 prongsDCA = np.linspace(1,2, 1)
 
 cent_classes = [[0,10], [10,30], [30,50]]
+n_ev = [11e6, 11e6*2, 11e6*2]
+
 mass_range = [2.96,3.04]
 n_bins = 40
 
 
-mc_file = uproot.open("/data/fmazzasc/PbPb_2body/SignalTable_20g7.root")
+mc_file = uproot.open("/data/fmazzasc/PbPb_2body/SignalTable_16h7abc.root")
 df = uproot.open("/data/fmazzasc/PbPb_2body/DataTable_15o.root")["DataTable"].arrays(library="pd")
 rec_df = mc_file["SignalTable"].arrays(library="pd")
 gen_df = mc_file["GenTable"].arrays(library="pd")
 
 ffile = ROOT.TFile("std_analysis.root", "recreate")
 
-for cent in cent_classes:
+for ind, cent in enumerate(cent_classes):
     ffile.mkdir(f'{cent[0]}_{cent[1]}')
     ffile.cd()
     for cosPA in cosPAcuts:
@@ -57,16 +59,13 @@ for cent in cent_classes:
                         cut = cut + f" and {cent[0]}<centrality<{cent[1]}"
                         print("#################################")
                         print("CUT: ", cut)
-                        print("EFFICIENCY: ", len(rec_df.query(cut + "and abs(Rapidity)<0.5"))/len(gen_df.query("abs(rapidity)<0.5" + f" and {cent[0]}<centrality<{cent[1]} and matter==1")))
+                        eff = len(rec_df.query(cut + "and abs(Rapidity)<0.5 and Matter==1"))/len(gen_df.query("abs(rapidity)<0.5" + f" and {cent[0]}<centrality<{cent[1]} and matter==1"))
+                        print("EFFICIENCY: ", eff)
                         df_sel = df.query(cut)
                         selected_data_hist = np.histogram(np.array(df_sel['m']), bins=n_bins, range=mass_range)                        
                         selected_data_hist = hp.h1_invmass(selected_data_hist[0], mass_range=mass_range, bins=n_bins, name=f'{cut}')
                         fit_result = hp.fit_hist(selected_data_hist, cent_class =cent, pt_range = [2,9], ct_range = [2,35])
+                        print('yield: ', fit_result[0]/n_ev[ind]/eff)
 
 
 ffile.Close()
-
-
-
-
-print("EFF2:",   len(rec_df.query("0<centrality<10 and pt>2 and abs(Rapidity)<0.5 "))/len(gen_df.query("0<centrality<10 and abs(rapidity)<0.5")) )

utils/helpers.py
@@ -11,6 +11,30 @@ import uproot
 import yaml
 
 
+def generate_th1(counts, bins, name=''):
+    th1 = ROOT.TH1D(f'{name}', f'{name}', len(bins) - 1, np.array(bins, 'double'))
+    for index in range(0, len(counts)):
+        th1.SetBinContent(index+1, counts[index])
+    th1.SetDirectory(0)
+    return th1
+
+def presel_eff_hist(df_list, col_name, split, cent_bins, bins):
+
+    counts_rec = np.histogram(df_list[0]["pt"], bins=bins)
+    counts_gen = np.histogram(df_list[1]["pt"], bins=bins)
+    eff = counts_rec[0]/counts_gen[0]
+    hist_eff = generate_th1(eff, bins, f"fPreselEff_vs_{col_name}_{split}_{cent_bins[0]}_{cent_bins[1]}")
+    hist_eff.GetXaxis().SetTitle('#it{p}_{T} (GeV/#it{c})')
+    hist_eff.GetYaxis().SetTitle('Efficiency')
+    hist_eff.SetMinimum(0)
+    hist_eff.SetDrawOption("histo")
+    hist_eff.SetLineWidth(2)
+
+    # return histogram
+    return hist_eff
+
+
+
 def ndarray2roo(ndarray, var):
     if isinstance(ndarray, ROOT.RooDataSet):
         print('Already a RooDataSet')
@@ -90,19 +114,74 @@ def bw_fit(histo, bw):
     # params[0] = 2.991
     pwg = ROOT.AliPWGFunc()
     bw = pwg.GetBGBW(params[0], params[1], params[2], params[3], params[4])
-    bw.SetParLimits(0, 2.990, 2.992)
+    bw.FixParameter(0,2.991)
     bw.SetParLimits(1, 0, 2)
     bw.SetParLimits(2, 0, 2)
     bw.SetParLimits(3, 0, 2)
     bw.SetParLimits(4, 0, 1000)
 
-    fit_result = histo.Fit(bw, "QSMI+")
+    fit_result = histo.Fit(bw, "QSMI+", "", 2,9)
     cov_matrix = fit_result.GetCovarianceMatrix()
-    par_after = bw.GetParameters()
-    integral = bw.Integral(0,20, 1e7)
-    print(cov_matrix[1][1])
+    integral = bw.Integral(0,20, 1e6)
     integral_error = bw.IntegralError(0,20, fit_result.GetParams(), cov_matrix.GetMatrixArray())
 
-    print("yield: ", integral, ", error: ", integral_error)
-    return histo, integral,integral_error
+    return histo, integral, integral_error, bw
     
+
+
+def mtexpo_fit(histo):
+
+    fit_function = ROOT.TF1('mtexpo', '[0]*x*TMath::Exp(-TMath::Sqrt(x**2 + 2.990**2)/[1])', 0, 10)
+    fit_function.SetParameter(0, 2e-5)
+    fit_function.SetParameter(1, 3)
+
+    fit_function.SetParLimits(0, 1e-6, 1e-4)
+    fit_function.SetParLimits(1, 1, 5)
+    # print(fit_function.GetParLimits(0))
+
+    fit_result = histo.Fit(fit_function, "SMI+")
+    cov_matrix = fit_result.GetCovarianceMatrix()
+    par_after = fit_function.GetParameters()
+    integral = fit_function.Integral(0,20)
+    integral_error = fit_function.IntegralError(0,20, fit_result.GetParams(), cov_matrix.GetMatrixArray())
+
+    print("yield: ", integral, ", error: ", integral_error)
+    return histo, integral, integral_error
+
+
+
+def reweight_efficiency(hEfficiencyAnalysisBins, hEfficiencySmallBins, fInputSpectrum):  
+    epsilon = 1e-5
+    for ibin in range(1,hEfficiencyAnalysisBins.GetNbinsX()+1):
+        c = hEfficiencyAnalysisBins.GetBinCenter(ibin)
+        w = hEfficiencyAnalysisBins.GetBinWidth(ibin)/2.0
+        x1 = c-w
+        x2 = c+w
+        n1 = hEfficiencySmallBins.FindBin(x1+epsilon)
+        n2 = hEfficiencySmallBins.FindBin(x2-epsilon)
+        num=0
+        den=0
+        deltaP_squared = 0
+        deltaG_squared = 0
+
+        for i in range(n1,n2+1):
+            x = hEfficiencySmallBins.GetBinCenter(i)
+            eps_i = hEfficiencySmallBins.GetBinContent(i)
+            gen_i = fInputSpectrum.Eval(x)
+            error_eps_i = hEfficiencySmallBins.GetBinError(i)
+            error_gen_i = 0  
+            deltaP_squared = deltaP_squared + (eps_i*error_gen_i)**2 + (error_eps_i*gen_i)**2
+            deltaG_squared = deltaG_squared + error_gen_i*error_gen_i
+            num = num + eps_i*gen_i
+            den = den + gen_i
+
+        average_eff = num/den if den!=0 else 0   
+
+        deltaP = np.sqrt(deltaP_squared)
+        deltaG = np.sqrt(deltaG_squared)
+        error_eff = np.sqrt((deltaP/den)**2 + (num*deltaG/(den*den))**2)
+
+        ##Fill Average Efficiency
+        hEfficiencyAnalysisBins.SetBinContent(ibin,average_eff)
+        hEfficiencyAnalysisBins.SetBinError(ibin,error_eff)
+